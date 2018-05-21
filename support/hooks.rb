205b94cb79b45aeff763969f4f9928997be2316d
Apzal Bahin <apzal.bn@gmail.com>
2018-05-21 14:03:38 +0530

    updated changes

--- support/hooks.rb
+++ support/hooks.rb
@@ -1,6 +1,7 @@
 Before do|scenario|
   puts scenario.name
   # @browser= Watir::Browser.new :chrome
+  ENV['PATH'] += ";#{Dir.pwd}/support/drivers/"
   @browser=Watir::Browser.new :chrome,options: {options: {'useAutomationExtension' => false}},:switches => %w[--no-sandbox --disable-notifications --disable-infobars --start-maximized]
   @browser.goto('https://www.google.com')
 end
