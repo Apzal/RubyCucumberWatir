429d743e9c8a0589b87b90ebcd401d3b76edf670
Apzal Bahin <apzal.bn@gmail.com>
2018-05-21 15:53:10 +0530

    updated changes

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -8,7 +8,9 @@ end
 
 Then(/^I should see result for (.*)$/) do|expected|
   sleep 4
-  result=on_page(SearchPage).india_search_element.text
+  puts "search_result#{@browser.link(:text=> expected).text}"
+  result=@browser.link(:text=> expected).text
+  puts "result is:#{result}"
   expect(result == expected).to eq(true),'Could not find the text'
 end
 
